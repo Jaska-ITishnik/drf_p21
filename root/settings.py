183e3ee60e3a07ccfa7e28de9ea5ea3a1593cf68
Jaska-ITishnik <jasurbekbekmirzayev2004@gmail.com>
2024-09-14 16:08:52 +0500

    cicd changes

--- root/settings.py
+++ root/settings.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
     # ]
 }
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Jasurbeks Project API',
+    'TITLE': 'Jasurbek API',
     'DESCRIPTION': 'IMPROVE MY DJANGO REST FRAMEWORK SKILLS🚀',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
